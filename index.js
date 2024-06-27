ad9367b2a37669f06717a01a4c1833762cdbda45
membae <membaphilip1@gmail.com>
2024-06-27 13:49:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName="bob";
+var bestCustomer=undefined;
+const leastFavoriteCustomer="mem";
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer="not bob";
+}
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="phil"
+}
